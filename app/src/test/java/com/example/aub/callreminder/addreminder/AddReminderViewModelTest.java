72eceaf41b5a9244670167d5635318fd97a2bc6c
aubdira <ayoubdiraoui@gmail.com>
2018-02-06 09:58:44 +0000

    Add testing dependencies

---
+++ app/src/test/java/com/example/aub/callreminder/addreminder/AddReminderViewModelTest.java
@@ -0,0 +1,13 @@
+package com.example.aub.callreminder.addreminder;
+
+import android.support.test.runner.AndroidJUnit4;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by aub on 2/5/18.
+ * Project: CallReminder
+ */
+@RunWith(AndroidJUnit4.class)
+public class AddReminderViewModelTest {
+    
+}
